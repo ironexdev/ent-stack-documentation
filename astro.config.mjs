6268142933521953a906d8224a5f12d941c40342
ironexdev <write.to.ironex@gmail.com>
2025-01-05 22:45:05 +0100

    Fixed: paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: "https://ironexdev.github.io",
-    base: "ent-stack-docs-private",
+    base: "ent-stack-documentation",
 
     integrations: [tailwind({
        configFile: "./tailwind.config.js",
     }), starlight({
-        title: "ENT Stack Docs",
+        title: "ENT Stack Documentation",
         social: {
             github: "https://github.com/withastro/starlight",
         },
