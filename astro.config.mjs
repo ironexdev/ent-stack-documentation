6a31d3a036ac5c468714b6f063f5743dba4d0247
ironexdev <write.to.ironex@gmail.com>
2025-01-12 22:04:11 +0100

    Fixed: routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: "https://ironexdev.github.io",
-    base: "ent-stack-documentation",
 
     integrations: [tailwind({
        configFile: "./tailwind.config.js",
