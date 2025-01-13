d5a15ee7a411ff7d910b632c5d3a3f279d5712d6
ironexdev <write.to.ironex@gmail.com>
2025-01-13 10:51:08 +0100

    Fixed: config url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import {defineConfig} from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-    site: "https://ironexdev.github.io",
+    site: "https://ent-stack.com",
 
     integrations: [tailwind({
        configFile: "./tailwind.config.js",
